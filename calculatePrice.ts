6e8ed6989cb07931ec0cf9af48436d8ff208b341
shimonhaga <shimon.haga@gmail.com>
2024-02-28 15:12:09 +0900

    消費税が8％に変更

--- calculatePrice.ts
+++ calculatePrice.ts
@@ -4,8 +4,8 @@ type Product = {
 };
 
 function calculatePrice(product: Product, quantity: number): number {
-  // 消費税3％導入 -> 5％に変更
-  return Math.ceil(product.price * quantity * 1.05);
+  // 消費税3％導入 -> 5％に変更 -> 8％に変更
+  return Math.ceil(product.price * quantity * 1.08);
 }
 
 export default calculatePrice;
