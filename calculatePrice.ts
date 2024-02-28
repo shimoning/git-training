ab59d54ef78d9d9f4a7c8e47ecc90f45d456d48e
shimonhaga <shimon.haga@gmail.com>
2024-02-28 15:31:45 +0900

    端数処理を拡張

--- calculatePrice.ts
+++ calculatePrice.ts
@@ -3,9 +3,25 @@ type Product = {
   price: number;
 };
 
-function calculatePrice(product: Product, quantity: number): number {
+enum Rounding {
+  ROUND = 0,
+  CEIL = 1,
+  FLOOR = 2,
+}
+
+function calculatePrice(product: Product, quantity: number, rounding: Rounding): number {
   // 消費税3％導入 -> 5％に変更
-  return Math.ceil(product.price * quantity * 1.05);
+  const price = product.price * quantity * 1.05;
+
+  // 端数処理
+  switch (rounding) {
+    case Rounding.ROUND:
+      return Math.round(price);
+    case Rounding.CEIL:
+      return Math.ceil(price);
+    case Rounding.FLOOR:
+      return Math.floor(price);
+  }
 }
 
 export default calculatePrice;
