fda00329b9892f994e8ec24555caac3bdcac1b48
shimonhaga <shimon.haga@gmail.com>
2024-02-28 15:45:44 +0900

    消費税率を定数化

--- calculatePrice.ts
+++ calculatePrice.ts
@@ -3,9 +3,12 @@ type Product = {
   price: number;
 };
 
+// 今後の増税に備えて消費税率を定数として定義
+const taxRate = 0.05;
+
 function calculatePrice(product: Product, quantity: number): number {
   // 消費税3％導入 -> 5％に変更
-  return Math.ceil(product.price * quantity * 1.05);
+  return Math.ceil(product.price * quantity * (1 + taxRate));
 }
 
 export default calculatePrice;
