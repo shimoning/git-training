c18bd40108b03a1370acc10b48cae376fb9a899e
shimonhaga <shimon.haga@gmail.com>
2024-02-28 15:20:36 +0900

    消費税10％と軽減税率の対応

--- calculatePrice.ts
+++ calculatePrice.ts
@@ -1,11 +1,15 @@
 type Product = {
   name: string;
   price: number;
+  isFood: boolean; // 食品かどうか
 };
 
 function calculatePrice(product: Product, quantity: number): number {
-  // 消費税3％導入 -> 5％に変更 -> 8％に変更
-  return Math.ceil(product.price * quantity * 1.08);
+  // 軽減税率対応
+  const taxRate = product.isFood
+    ? 1.08 // 食品は8%税率
+    : 1.1; // 食品以外は10%税率
+  return Math.ceil(product.price * quantity * taxRate);
 }
 
 export default calculatePrice;
